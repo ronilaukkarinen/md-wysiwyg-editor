50d0dfbe14641d60d2f77cf75e616fe196822c3e
Aly W <alyw131@gmail.com>
2020-09-12 05:18:12 -0700

    .

--- src/file-manager.js
+++ src/file-manager.js
@@ -10,66 +10,109 @@ import {
   fileSave,
 } from 'https://unpkg.com/browser-nativefs';
 
-(async () => {
-  // Open a file.
-  const blob = await fileOpen({
-    mimeTypes: ['image/*'],
-  });
+function bnfs-open-file() {
 
-  // Open multiple files.
-  const blobs = await fileOpen({
-    mimeTypes: ['image/*'],
-    multiple: true,
-  });
+  (async () => {
 
-  // Open all files in a directory,
-  // recursively including subdirectories.
-  const blobsInDirectory = await directoryOpen({
-    recursive: true,
-  });
+    // Options are optional.
+    const options = {
+      // List of allowed MIME types, defaults to `*/*`.
+      mimeTypes: ['image/*'],
+      // List of allowed file extensions (with leading '.'), defaults to `''`.
+      extensions: ['.png', '.jpg', '.jpeg', '.webp'],
+      // Textual description for file dialog , defaults to `''`.
+      description: 'Image files',
+    };
+    
+    const blob = await fileOpen(options);
 
-  // Save a file.
-  await fileSave(blob, {
-    fileName: 'Untitled.png',
-    extensions: ['.png'],
-  });
-})();
+  })();
 
-(async () => {
+  return blob;
+}
 
-  /* Opening files */
-  
-  // Options are optional.
-  const options = {
-    // List of allowed MIME types, defaults to `*/*`.
-    mimeTypes: ['image/*'],
-    // List of allowed file extensions (with leading '.'), defaults to `''`.
-    extensions: ['.png', '.jpg', '.jpeg', '.webp'],
-    // Set to `true` for allowing multiple files, defaults to `false`.
-    multiple: true,
-    // Textual description for file dialog , defaults to `''`.
-    description: 'Image files',
-  };
-  
-  const blobs = await fileOpen(options);
-  
-  /* Saving files */
-  
-  // Options are optional.
-  const options_ = {
-    // Suggested file name to use, defaults to `''`.
-    fileName: 'Untitled.txt',
-    // Suggested file extensions (with leading '.'), defaults to `''`.
-    extensions: ['.txt'],
-  };
-  
-  // Optional file handle to save back to an existing file.
-  // This will only work with the Native File System API.
-  // Get a `FileHandle` from the `handle` property of the `Blob`
-  // you receive from `fileOpen()` (this is non-standard).
-  const handle = previouslyOpenedBlob.handle;
-  
-  await fileSave(someBlob, options_, handle);
+function bnfs-open-files() {
 
-})();
+  (async () => {
+
+    // Options are optional.
+    const options = {
+      // List of allowed MIME types, defaults to `*/*`.
+      mimeTypes: ['image/*'],
+      // List of allowed file extensions (with leading '.'), defaults to `''`.
+      extensions: ['.png', '.jpg', '.jpeg', '.webp'],
+      // Set to `true` for allowing multiple files, defaults to `false`.
+      multiple: true,
+      // Textual description for file dialog , defaults to `''`.
+      description: 'Image files',
+    };
+    
+    const blobs = await fileOpen(options);
+
+  })();
+
+  return blobs;
+}
+
+function bnfs-open-directory() {
+
+  (async () => {
+
+    // Options are optional.
+    const options = {
+      recursive: true,
+    };
+    
+    // Open all files in a directory,
+    // recursively including subdirectories.
+    const blobsInDirectory = await directoryOpen(options);
+
+  })();
+
+  return blobsInDirectory;
+}
+
+function bnfs-save-file() {
+
+  (async () => {
+  
+    // Options are optional.
+    const options_ = {
+      // Suggested file name to use, defaults to `''`.
+      fileName: 'Untitled.txt',
+      // Suggested file extensions (with leading '.'), defaults to `''`.
+      extensions: ['.txt'],
+    };
+    
+    // Optional file handle to save back to an existing file.
+    // This will only work with the Native File System API.
+    // Get a `FileHandle` from the `handle` property of the `Blob`
+    // you receive from `fileOpen()` (this is non-standard).
+    const handle = previouslyOpenedBlob.handle;
+    
+    await fileSave(someBlob, options_, handle);
+
+  })();
+
+  return;
+}
+
+function bnfs-save-file-as() {
+
+  (async () => {
+  
+    // Options are optional.
+    const options_ = {
+      // Suggested file name to use, defaults to `''`.
+      fileName: 'Untitled.txt',
+      // Suggested file extensions (with leading '.'), defaults to `''`.
+      extensions: ['.txt'],
+    };
+
+    await fileSave(someBlob, options_);
+
+  })();
+
+  return;
+}
 
