83d51db15564fe5e922c3dc73641803054cc1173
Aly W <alyw131@gmail.com>
2021-02-20 03:55:46 -0800

    .

--- libs/turndown.js
+++ libs/turndown.js
@@ -117,7 +117,7 @@ var TurndownService = (function () {
       if (parent.nodeName === 'OL') {
         var start = parent.getAttribute('start');
         var index = Array.prototype.indexOf.call(parent.children, node);
-        prefix = (start ? Number(start) + index : index + 1) + '.';
+        prefix = (start ? Number(start) + index : index + 1) + '. ';
       }
       return (
         prefix + content + (node.nextSibling && !/\n$/.test(content) ? '\n' : '')
